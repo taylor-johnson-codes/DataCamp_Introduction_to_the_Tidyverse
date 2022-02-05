9f474b259cce5f61429ead2a5bee2eeb3bb4fa42
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-05 13:29:00 -0800

    added code to 1_data wrangling R file

---
+++ 1_Data wrangling.R
@@ -0,0 +1,43 @@
+# Before you can work with the gapminder dataset, you'll need to load two R packages that contain the tools for working with it, 
+# then display the gapminder dataset so that you can see what it contains.
+
+# Install and load the gapminder package
+install.packages("gapminder")
+library(gapminder)
+
+# Load the dplyr package
+library(dplyr)
+
+# Look at the gapminder dataset
+gapminder
+
+# Filter the gapminder dataset for the year 1957
+gapminder %>%
+  filter(year == 1957)
+
+# Filter for China in 2002
+gapminder %>%
+  filter(year==2002, country=="China")
+
+# Sort in ascending order of lifeExp
+gapminder %>% arrange(lifeExp)
+
+# Sort in descending order of lifeExp
+gapminder %>% arrange(desc(lifeExp))
+
+# Filter for the year 1957, then arrange in descending order of population
+gapminder %>% 
+  filter(year==1957) %>%
+  arrange(desc(pop))
+
+# Use mutate to change lifeExp to be in months
+gapminder %>% mutate(lifeExp = 12 * lifeExp)
+
+# Use mutate to create a new column called lifeExpMonths
+gapminder %>% mutate(lifeExpMonths = 12 * lifeExp)
+
+# Filter, mutate, and arrange the gapminder dataset
+gapminder %>% 
+  filter(year==2007) %>% 
+  mutate(lifeExpMonths = 12*lifeExp) %>% 
+  arrange(desc(lifeExpMonths))
